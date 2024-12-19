6a8cd6d27043cae051227a5823efb98431287b6d
gabriel-r-s <author@email>
2024-12-19 09:39:39 -0300

    manter aspecto da imagem e centralizar

--- src/app.hpp
+++ src/app.hpp
@@ -6,12 +6,14 @@
 class App {
     Sdl *sdl;
     SDL_Texture *image, *processed;
+    SDL_Rect image_rect;
     std::vector<bool> kernel;
     size_t kernel_width;
 
   public:
     App(Sdl *sdl)
-        : sdl(sdl), image(nullptr), processed(nullptr), kernel_width(3) {
+        : sdl(sdl), image(nullptr), processed(nullptr),
+          image_rect({0, 0, 0, 0}), kernel_width(3) {
         kernel = {0, 1, 0, 1, 1, 1, 0, 1, 0};
     }
 
@@ -24,6 +26,17 @@ class App {
         SDL_SetRenderTarget(sdl->renderer, processed);
         SDL_RenderCopy(sdl->renderer, image, nullptr, nullptr);
         SDL_SetRenderTarget(sdl->renderer, nullptr);
+    }
+
+    void adjust_image_rect(int w, int h) {
+        float ratio = float(h) / sdl->height;
+
+        float new_w = w / ratio;
+        float new_h = h / ratio;
+        float new_x = (sdl->width - new_w) / 2;
+        float new_y = (sdl->height - new_h) / 2;
+
+        image_rect = {int(new_x), int(new_y), int(new_w), int(new_h)};
     }
 
     bool load_image(const char *path) {
@@ -39,6 +52,7 @@ class App {
         SDL_QueryTexture(image, nullptr, nullptr, &w, &h);
         processed = SDL_CreateTexture(sdl->renderer, SDL_PIXELFORMAT_RGBA8888,
                                       SDL_TEXTUREACCESS_TARGET, w, h);
+        adjust_image_rect(w, h);
         reset_image();
         return true;
     }
@@ -47,6 +61,6 @@ class App {
         if (processed == nullptr) {
             return;
         }
-        SDL_RenderCopy(sdl->renderer, processed, nullptr, nullptr);
+        SDL_RenderCopy(sdl->renderer, processed, nullptr, &image_rect);
     }
 };
